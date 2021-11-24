8946479473a178b78ef8ff2430c40d138ee2d3bf
Meheraj <meherajmahmmd@gmail.com>
2021-11-24 22:43:53 +0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,20 @@ import Home from "./pages/Home";
 import SignIn from "./pages/Auth/SignIn";
 import SignUp from "./pages/Auth/SignUp";
 import Profile from "./pages/User/Profile";
+import Navbar from "./components/Navbar";
+import EditProfile from "./pages/User/EditProfile";
 
 function App() {
   return (
     <Router>
       <main>
+        <Navbar />
         <Routes>
           <Route path="/" exact element={<Home />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/edit-profile" element={<EditProfile />} />
         </Routes>
       </main>
     </Router>
