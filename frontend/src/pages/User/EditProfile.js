a80422e39165eb5c8c57b04102b6eda3679a77f5
Meheraj <meherajmahmmd@gmail.com>
2021-11-24 22:44:21 +0600

    Create EditProfile.js

---
+++ frontend/src/pages/User/EditProfile.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function EditProfile() {
+  return (
+    <div>
+      <div className="container mt-3">
+        <h3 className="text-center">Edit Profile</h3>
+        <hr />
+        <div className="row"></div>
+      </div>
+    </div>
+  );
+}
+
+export default EditProfile;
