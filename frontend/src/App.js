c62dafee0c84b8b0f9a135eba2a5235549a10887
Meheraj <meherajmahmmd@gmail.com>
2021-11-24 22:23:08 +0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignIn from "./pages/Auth/SignIn";
 import SignUp from "./pages/Auth/SignUp";
+import Profile from "./pages/User/Profile";
+
 function App() {
   return (
     <Router>
@@ -10,6 +12,7 @@ function App() {
           <Route path="/" exact element={<Home />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </main>
     </Router>
