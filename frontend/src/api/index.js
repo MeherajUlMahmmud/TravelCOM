3a11213c25338147fa80b2adc3075ba256d4b1b9
Meheraj <meherajmahmmd@gmail.com>
2021-12-02 21:41:07 +0600

    Create index.js

---
+++ frontend/src/api/index.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const url = "http://localhost:5000";
+
+export const createUser = (user) => {
+    return axios.post(`${url}/create-user`, user);
+};
+
+export const getAllUsers = () => {
+    return axios.get(`${url}/`);
+};
+
+export const getUserDetails = (userId) => {
+    return axios.get(`${url}/user/${userId}`);
+};
+
+export const fetchUpcomingTours = () => {
+  return axios.get(url);
+};
+
+export const createPost = (post) => {
+  return axios.post(url, post);
+};
+
+export const updatePost = (id, post) => {
+  return axios.patch(`${url}/${id}`, post);
+};
+
+export const deletePost = (id) => {
+  return axios.delete(`${url}/${id}`);
+};
+
+export const likePost = (id) => {
+  return axios.patch(`${url}/${id}/likePost`);
+};
