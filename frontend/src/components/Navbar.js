3028a488e8f1e85d94a26defe186cf1455d3031f
Meheraj <meherajmahmmd@gmail.com>
2021-12-06 16:29:41 +0600

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { auth } from "../firebase-config";
 import { signOut } from "firebase/auth";
-import { getUserDetails } from "../api/index";
 
 function Navbar({ user }) {
   const [name, setName] = useState("");
   const [role, setRole] = useState("");
+
   useEffect(() => {
     if (user !== null) {
       setName(localStorage.getItem("name"));
