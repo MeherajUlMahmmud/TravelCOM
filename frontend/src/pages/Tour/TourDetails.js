56f777d98a1ac6bcf205fd0b40e393ab1c9e5b16
Meheraj <meherajmahmmd@gmail.com>
2021-12-06 16:29:12 +0600

    Update TourDetails.js

--- frontend/src/pages/Tour/TourDetails.js
+++ frontend/src/pages/Tour/TourDetails.js
@@ -1,91 +1,101 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getTourDetails } from "../../api";
 import Footer from "../../components/Footer";
 
 function TourDetails() {
+  const tourId = window.location.pathname.split("/")[2];
+  const userId = localStorage.getItem("id");
+  const role = localStorage.getItem("role");
+  const [tour, setTour] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getTourDetails(tourId).then((res) => {
+      console.log(res.data["data"]);
+      setTour(res.data["data"]);
+      convertDate(res.data["data"]);
+      setLoading(false);
+    });
+    return () => {};
+  }, []);
+
+  const convertDate = (tour) => {
+    tour.startDate = new Date(tour.startDate).toLocaleDateString("en-GB", {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    });
+    tour.endDate = new Date(tour.endDate).toLocaleDateString("en-GB", {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    });
+  };
+
   return (
     <div>
-      <div className="container mb-5 mt-5">
-        <div className="row">
-          <div className="col-md-12">
-            <div className="text-center mb-5">
-              <img
-                src="https://mdbootstrap.com/img/new/standard/nature/111.jpg"
-                className="img-fluid mb-3"
-              />
-              <h2>Rangamati Tour 2021</h2>
-              <hr />
+      {loading ? (
+        <div className="d-flex justify-content-center m-5">
+          <div className="spinner-border" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+      ) : (
+        <div className="container mb-5 mt-5">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="text-center mb-5">
+                <img
+                  src={
+                    tour.image ||
+                    "https://mdbootstrap.com/img/new/standard/nature/111.jpg"
+                  }
+                  className="img-fluid mb-3"
+                  alt=""
+                />
+                <h2>{tour.name}</h2>
+                {tour.userId === userId ? (
+                  <div className="row">
+                    <div className="col-md-6">
+                      <a className="btn btn-info btn-md">Edit Deails</a>
+                    </div>
+                    <div className="col-md-6">
+                      <a className="btn btn-danger btn-md">Delete</a>
+                    </div>
+                  </div>
+                ) : null}
+                {userId && role === "user" ? (
+                  <div className="row">
+                    <div className="col-md-12">
+                      <div className="text-center">
+                        <a className="btn btn-success btn-md">BOOK NOW</a>
+                      </div>
+                    </div>
+                  </div>
+                ) : null}
+                <hr />
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4">
+              <h4>Tour Starts On: {tour.startDate}</h4>
+            </div>
+            <div className="col-md-4">
+              <h4>Tour Ends On: {tour.endDate}</h4>
+            </div>
+            <div className="col-md-4">
+              <h4>Tour Duration: {tour.duration} Days</h4>
+            </div>
+          </div>
+          <div className="row mt-2">
+            <div className="col-md-12">
+              <h5>Tour Details</h5>
+              <p>{tour.description}</p>
             </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col-md-4">
-            <h4>Tour Starts On: 7th December 2021</h4>
-          </div>
-          <div className="col-md-4">
-            <h4>Tour Ends On: 10th December 2021</h4>
-          </div>
-          <div className="col-md-4">
-            <h4>Tour Duration: 4 Days</h4>
-          </div>
-        </div>
-        <div className="row mt-2">
-          <div className="col-md-12">
-            <h5>Tour Details</h5>
-            <p className="justify">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-              scelerisque enim at metus consequat vehicula. Fusce egestas enim
-              sed nibh hendrerit, nec vestibulum quam ullamcorper. Proin
-              tincidunt lacinia bibendum. Phasellus aliquam egestas placerat.
-              Sed ut lorem vitae magna gravida fringilla malesuada auctor quam.
-              Duis dignissim viverra mi, eget pulvinar nisi rutrum a.
-              Suspendisse auctor congue ante, luctus tempus nibh lacinia a.
-              Class aptent taciti sociosqu ad litora torquent per conubia
-              nostra, per inceptos himenaeos. Duis ac ullamcorper massa.
-              Interdum et malesuada fames ac ante ipsum primis in faucibus.
-              Integer imperdiet sapien diam, ut blandit lorem hendrerit sed. In
-              hac habitasse platea dictumst. Maecenas finibus dui in enim
-              malesuada ultricies. Nam quam erat, pharetra vitae congue eu,
-              rutrum ut libero. Vivamus ac ante at velit commodo ornare. Vivamus
-              at molestie quam. Ut varius commodo dolor, quis dictum nisl
-              pulvinar eu. Quisque ut justo efficitur, ultricies odio ut,
-              pretium enim. In dui arcu, laoreet at tempor at, pellentesque sed
-              neque. Vestibulum aliquam semper magna, vitae ultrices sem porta
-              sed. Quisque vehicula finibus tincidunt. In vel magna mollis,
-              condimentum est sit amet, iaculis lectus. Curabitur malesuada
-              mattis metus sed aliquet. Pellentesque sed nulla in lacus
-              pellentesque imperdiet nec quis eros. Ut eget ipsum eget lectus
-              rutrum ultricies. Cras in ultrices justo, in hendrerit dolor.
-              Aliquam sodales, mi sit amet feugiat mollis, sem quam tempus
-              risus, ut porttitor diam risus non est. Etiam ut mauris in urna
-              malesuada vulputate. Pellentesque et quam a metus ullamcorper
-              pulvinar. Quisque purus augue, tincidunt at mattis hendrerit,
-              varius quis augue. Morbi sed mi sed odio volutpat vulputate.
-              Phasellus tincidunt facilisis quam tincidunt tincidunt. In
-              pellentesque sem id ultricies consequat. Morbi sapien urna,
-              elementum et felis non, mollis mollis magna. Duis ut erat nunc.
-              Maecenas laoreet rhoncus ante, elementum maximus elit finibus non.
-              Duis at neque lacus. Fusce sodales tempus lacus quis tempor. Nulla
-              lacus sem, imperdiet eu iaculis eu, finibus sit amet mi. Interdum
-              et malesuada fames ac ante ipsum primis in faucibus. Vestibulum
-              scelerisque tellus ac erat tincidunt dignissim. Etiam egestas ut
-              mauris eget sodales. Duis et ex sit amet justo porta iaculis.
-              Pellentesque hendrerit eleifend aliquam. Donec cursus ex lacus,
-              eget ullamcorper felis tempus hendrerit. Nullam efficitur, elit ac
-              blandit laoreet, dui urna pulvinar purus, a euismod nibh augue
-              eget orci. Ut maximus augue lectus, eu vehicula libero pretium sit
-              amet. Nam ultricies est viverra ante eleifend, ac iaculis massa
-              vehicula. Nam vitae massa magna. Pellentesque habitant morbi
-              tristique senectus et netus et malesuada fames ac turpis egestas.
-              Sed egestas orci quis dolor aliquet fermentum. Vestibulum accumsan
-              nisl id ipsum sodales porta. Pellentesque gravida justo lacus, nec
-              ultricies mi pharetra eu. Donec volutpat sagittis est, et euismod
-              lacus lobortis eu. Quisque fermentum enim elementum interdum
-              hendrerit. Aliquam faucibus nibh eu odio egestas, vel euismod odio
-              consequat.
-            </p>
-          </div>
-        </div>
-      </div>
+      )}
       <Footer />
     </div>
   );
