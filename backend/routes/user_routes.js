4cfedbc11211c1dd21ebb56f9122d62798281968
Meheraj <meherajmahmmd@gmail.com>
2021-12-06 09:15:18 +0600

    Update user_routes.js

--- backend/routes/user_routes.js
+++ backend/routes/user_routes.js
@@ -1,12 +1,11 @@
 import express from "express";
-import { getAllUsers, createUser, getUserDetails } from "../controllers/user_controllers.js";
+import { getAllUsers, createUser, getUserDetails, updateUser } from "../controllers/user_controllers.js";
 
 const router = express.Router();
 
-
 router.get("/", getAllUsers);
 router.post("/create-user", createUser);
-// // router.patch("/update-user/:id", userController.updateUser);
+router.patch("/update-user/:id", updateUser);
 router.get("/user/:uid", getUserDetails);
 
 export default router;
