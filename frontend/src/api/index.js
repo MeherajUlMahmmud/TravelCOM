bf1f5434300b9c0c5a05a2603163c0f0c5a49adf
Meheraj <meherajmahmmd@gmail.com>
2021-12-12 03:53:21 +0600

    Update index.js

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -26,14 +26,41 @@ export const getUpcomingTours = () => {
   return axios.get(`${url}/tour/upcoming-tours`);
 };
 
+export const getBookingClosedTours = () => {
+  return axios.get(`${url}/tour/booking-closed`);
+};
+
+export const getCompletedTours = () => {
+  return axios.get(`${url}/tour/completed-tours`);
+};
+
 export const getTourDetails = (tourId) => {
   return axios.get(`${url}/tour/${tourId}`);
 };
 
-export const updateTour = (id, post) => {
-  return axios.patch(`${url}/${id}`, post);
+export const updateTour = (tour) => {
+  console.log(tour);
+  return axios.patch(`${url}/tour/update-tour/${tour.tourId}`, tour);
 };
 
 export const deleteTour = (id) => {
-  return axios.delete(`${url}/${id}`);
+  console.log(id);
+  return axios.delete(`${url}/tour/delete-tour/${id}`);
+};
+
+export const bookingTour = (booking) => {
+  return axios.post(`${url}/tour/booking-tour/${booking.tourId}`, booking);
+};
+
+export const closeBooking = (tourId) => {
+  return axios.patch(`${url}/tour/close-booking/${tourId}`);
+};
+
+export const completeTour = (tourId) => {
+  console.log(tourId);
+  return axios.patch(`${url}/tour/complete-tour/${tourId}`);
+};
+
+export const getBookedTourByTourIdAndUserId = (data) => {
+  return axios.get(`${url}/tour/booked-tour/${data.tourId}/${data.userId}`, data);
 };
