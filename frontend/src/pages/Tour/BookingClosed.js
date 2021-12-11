363317fb4e20144e846b170e4441e9450f314699
Meheraj <meherajmahmmd@gmail.com>
2021-12-12 03:16:01 +0600

    Update BookingClosed.js

--- frontend/src/pages/Tour/BookingClosed.js
+++ frontend/src/pages/Tour/BookingClosed.js
@@ -1,76 +1,91 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { getBookingClosedTours } from "../../api";
 import Footer from "../../components/Footer";
 
 function BookingClosed() {
-  let cardDetails = [];
+  const [tours, setTours] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  let N = 20;
-  for (let i = 1; i < N; i++) {
-    cardDetails.push({
-      id: i,
-      title: "Card Title " + i,
-      text: "Card Text " + i + " write something for card",
-      url: i,
-      button: "Button " + String(i),
+  useEffect(() => {
+    getBookingClosedTours().then((res) => {
+      console.log(res.data["data"]);
+      setTours(res.data["data"]);
+      console.log(tours);
+      setLoading(false);
     });
-  }
+  }, []);
+
   return (
     <div>
-      <div className="container mb-5">
-        <div className="row">
-          <div className="col-md-12">
-            <div className="text-center m-5">
-              <h1>Booking Closed Tours</h1>
-              <hr />
-            </div>
-            <div className="row">
-              <div className="container-fluid d-flex justify-content-center">
-                <div className="row row-cols-1 row-cols-md-2 g-4">
-                  {cardDetails.map((oneCard) => (
-                    <div
-                      className="card border m-2"
-                      key={oneCard.id}
-                      style={{
-                        maxWidth: "40rem",
-                      }}
-                    >
+      {loading ? (
+        <div className="d-flex justify-content-center m-5">
+          <div className="spinner-border" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+      ) : (
+        <div className="container mb-5">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="text-center m-5">
+                <h1>Booking Closed Tours</h1>
+                <hr />
+              </div>
+              <div className="row">
+                <div className="container-fluid d-flex justify-content-center">
+                  <div className="row row-cols-1 row-cols-md-2 g-4">
+                    {tours.map((oneCard) => (
                       <div
-                        className="bg-image hover-overlay ripple"
-                        data-mdb-ripple-color="light"
+                        className="card border-0 shadow-sm mb-4"
+                        key={oneCard.id}
+                        style={{
+                          maxWidth: "40rem",
+                          minWidth: "35rem",
+                        }}
                       >
-                        <img
-                          src="https://mdbootstrap.com/img/new/standard/nature/111.jpg"
-                          className="img-fluid"
-                          alt=""
-                        />
-                        <a href="#!">
-                          <div
-                            className="mask"
-                            style={{
-                              backgroundColor: "rgba(251, 251, 251, 0.15)",
-                            }}
-                          ></div>
-                        </a>
-                      </div>
-                      <div className="card-body">
-                        <h5 className="card-title">{oneCard.title}</h5>
-                        <p className="card-text">{oneCard.text}</p>
-                        <Link
-                          className="btn btn-outline-primary"
-                          to={oneCard.url}
+                        <div
+                          className="bg-image hover-overlay ripple"
+                          data-mdb-ripple-color="light"
                         >
-                          {oneCard.button}
-                        </Link>
+                          <img
+                            src={
+                              oneCard.image ||
+                              "https://mdbootstrap.com/img/new/standard/nature/111.jpg"
+                            }
+                            className="img-fluid"
+                            alt={oneCard.name}
+                          />
+                          <Link to={`/tour/${oneCard._id}`}>
+                            <div
+                              className="mask"
+                              style={{
+                                backgroundColor: "rgba(251, 251, 251, 0.15)",
+                              }}
+                            ></div>
+                          </Link>
+                        </div>
+                        <div className="card-body">
+                          <h5 className="card-title">{oneCard.name}</h5>
+                          <p className="card-text">
+                            {oneCard.description.substring(0, 100)}
+                          </p>
+                          <Link
+                            className="btn btn-outline-primary"
+                            to={`/tour/${oneCard._id}`}
+                          >
+                            See Details
+                          </Link>
+                        </div>
                       </div>
-                    </div>
-                  ))}
+                    ))}
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
       <Footer />
     </div>
   );
