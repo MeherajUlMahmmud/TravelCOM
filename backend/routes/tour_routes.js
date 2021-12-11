fdc36d6b6e58634d2e9af5d4266be238c311e4fc
Meheraj <meherajmahmmd@gmail.com>
2021-12-12 03:53:17 +0600

    Update tour_routes.js

--- backend/routes/tour_routes.js
+++ backend/routes/tour_routes.js
@@ -1,16 +1,32 @@
 import express from "express";
-import { createTour, getAllTours, getUpcomingTours, getSingleTour } from "../controllers/tour_controllers.js";
-
+import {
+  createTour,
+  updateTour,
+  deleteTour,
+  getAllTours,
+  getUpcomingTours,
+  getBookingClosedTours,
+  getCompletedTours,
+  getSingleTour,
+  bookingTour,
+  closeBooking,
+  completeTour,
+  getBookedTourByTourIdAndUserId,
+} from "../controllers/tour_controllers.js";
 
 const router = express.Router();
 
 router.route("/").get(getAllTours);
 router.get("/upcoming-tours", getUpcomingTours);
-// router.get("/booking-closed", tourController.getBookingClosedTours);
-// router.get("/completed-tours", tourController.getCompletedTours);
+router.get("/booking-closed", getBookingClosedTours);
+router.get("/completed-tours", getCompletedTours);
 router.get("/:id", getSingleTour);
 router.post("/create-tour", createTour);
-// tourRouter.patch("/update-tour/:id", tourController.updateTour);
-// tourRouter.delete("/delete-tour/:id", tourController.deleteTour);
+router.patch("/update-tour/:id", updateTour);
+router.delete("/delete-tour/:id", deleteTour);
+router.post("/booking-tour/:id", bookingTour);
+router.patch("/close-booking/:id", closeBooking);
+router.patch("/complete-tour/:id", completeTour);
+router.get("/booked-tour/:tourId/:userId", getBookedTourByTourIdAndUserId);
 
 export default router;
