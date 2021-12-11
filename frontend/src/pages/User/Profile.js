3f133aeef5ec058b0eecd53d8934ee5001a7fb1b
Meheraj <meherajmahmmd@gmail.com>
2021-12-11 21:39:28 +0600

    Update Profile.js

--- frontend/src/pages/User/Profile.js
+++ frontend/src/pages/User/Profile.js
@@ -35,96 +35,104 @@ function Profile() {
 
   return (
     <div>
-      <div className="container mt-3 mb-5">
-        <div className="row">
-          <div className="col-md-12">
-            <div className="text-center">
-              {picture ? (
-                <img
-                  src={picture}
-                  className="rounded-circle"
-                  alt="avatar"
-                  style={{ width: "200px" }}
-                />
-              ) : (
-                <img
-                  src={picture || "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200"}
-                  className="rounded-circle"
-                  alt="avatar"
-                  style={{ width: "200px" }}
-                />
-              )}
-              <h3>
-                <b>{name}</b>{" "}
-              </h3>
-              {role !== "user" ? (
-                <p>
-                  {role === "coordinator" ? "Tour Coordinator" : "Tour Guide"}
-                </p>
-              ) : null}
-              <h5>
-                <b>{bio}</b>
-              </h5>
+      {uid !== null ? (
+        <div className="container mt-3 mb-5">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="text-center">
+                {picture ? (
+                  <img
+                    src={picture}
+                    className="rounded-circle"
+                    alt="avatar"
+                    style={{ width: "200px" }}
+                  />
+                ) : (
+                  <img
+                    src={
+                      picture ||
+                      "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200"
+                    }
+                    className="rounded-circle"
+                    alt="avatar"
+                    style={{ width: "200px" }}
+                  />
+                )}
+                <h3>
+                  <b>{name}</b>{" "}
+                </h3>
+                {role !== "user" ? (
+                  <p>
+                    {role === "coordinator" ? "Tour Coordinator" : "Tour Guide"}
+                  </p>
+                ) : null}
+                <h5>
+                  <b>{bio}</b>
+                </h5>
 
-              {profession ? (
-                <p>
-                  <i className="fa fa-briefcase" aria-hidden="true"></i>{" "}
-                  <span>{profession}</span>
-                </p>
-              ) : null}
+                {profession ? (
+                  <p>
+                    <i className="fa fa-briefcase" aria-hidden="true"></i>{" "}
+                    <span>{profession}</span>
+                  </p>
+                ) : null}
 
-              {location ? (
-                <p>
-                  <i className="fa fa-map-marker" aria-hidden="true"></i>{" "}
-                  <span>{location}</span>
-                </p>
-              ) : null}
+                {location ? (
+                  <p>
+                    <i className="fa fa-map-marker" aria-hidden="true"></i>{" "}
+                    <span>{location}</span>
+                  </p>
+                ) : null}
 
-              <div className="row">
-                <div className="col-md-12">
-                  {facebook ? (
-                    <a
-                      href="https://www.facebook.com"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <i className="fab fa-facebook fa-2x"></i>
-                    </a>
-                  ) : null}
-                  <span> </span>
-                  {instagram ? (
-                    <a
-                      href="https://www.instagram.com"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <i className="fab fa-instagram fa-2x"></i>
-                    </a>
-                  ) : null}
+                <div className="row">
+                  <div className="col-md-12">
+                    {facebook ? (
+                      <a
+                        className="btn btn-primary btn-lg btn-floating"
+                        style={{
+                          backgroundColor: "#3b5998",
+                        }}
+                        href={facebook}
+                        target="_blank"
+                        rel="noreferrer"
+                        role="button"
+                      >
+                        <i className="fab fa-facebook fa-2x"></i>
+                      </a>
+                    ) : null}
+                    <span> </span>
+                    {instagram ? (
+                      <a
+                        className="btn btn-primary btn-lg btn-floating"
+                        style={{
+                          backgroundColor: "#ac2bac",
+                        }}
+                        href={instagram}
+                        target="_blank"
+                        rel="noreferrer"
+                        role="button"
+                      >
+                        <i className="fab fa-instagram fa-2x"></i>
+                      </a>
+                    ) : null}
+                  </div>
                 </div>
-              </div>
 
-              <Link to="/edit-profile">
-                <button className="btn btn-info btn-md m-3">
-                  Edit Profile
-                </button>
-              </Link>
+                <Link to="/edit-profile">
+                  <button className="btn btn-info btn-md m-3">
+                    Edit Profile
+                  </button>
+                </Link>
+              </div>
             </div>
-          </div>
-          <hr />
-        </div>
-        <div className="row">
-          <div className="col-md-12">
-            <h4>Booked Tours</h4>
+            <hr />
           </div>
         </div>
-        <hr />
-        <div className="row">
-          <div className="col-md-12">
-            <h4>Attended Tours</h4>
-          </div>
-        </div>
-      </div>
+      ) : (
+        <h3 className="text-center">
+          You are not authorized to view this page
+        </h3>
+      )}
       <Footer />
     </div>
   );
