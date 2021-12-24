1718a33ef6100b344bfb591ecf7873f1d0d3a8c3
Meheraj <meherajmahmmd@gmail.com>
2021-12-24 21:29:03 +0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,13 @@ dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
+
 
 app.use("/", userRoutes);
 app.use("/tour", tourRoutes);
